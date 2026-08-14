0a732bfa372c5b9a6c77fd67454e85f1cc03a4cc
Guilherme Polinfo <79385518+Polinfo-Informatica@users.noreply.github.com>
2026-08-14 16:34:33 -0300

    Enable strict lint and Prettier integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,25 @@
 import js from "@eslint/js";
 import globals from "globals";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+
 export default [
   { ignores: ["types/**"] },
   js.configs.recommended,
   {
     files: ["**/*.js", "**/*.mjs"],
-    languageOptions: { ecmaVersion: 2022, sourceType: "module", globals: { ...globals.es2021, ...globals.node } },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: { ...globals.es2021, ...globals.node },
+    },
     rules: {
-      "no-empty": "off",
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }]
-    }
-  }
+      "no-empty": ["error", { allowEmptyCatch: false }],
+      "no-constant-condition": ["error", { checkLoops: true }],
+      "no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  prettierRecommended,
 ];
